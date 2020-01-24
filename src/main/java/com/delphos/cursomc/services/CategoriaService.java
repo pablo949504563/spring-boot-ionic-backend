dcff4c6264ab9515a9085ccac4c0ffeb13339f0c
pablo gonzalez <pablo949504563@gmail.com>
2020-01-24 02:26:40 -0300

    validação sintatica com Bean validation

--- src/main/java/com/delphos/cursomc/services/CategoriaService.java
+++ src/main/java/com/delphos/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.delphos.cursomc.domain.Categoria;
+import com.delphos.cursomc.dto.CategoriaDTO;
 import com.delphos.cursomc.repositories.CategoriaRepository;
 import com.delphos.cursomc.services.exception.DataIntegrityException;
 import com.delphos.cursomc.services.exception.ObjectNotFoundException;
@@ -54,6 +55,12 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		
+	return new Categoria(objDto.getId(), objDto.getNome());
+	
 	}
 	
 }
