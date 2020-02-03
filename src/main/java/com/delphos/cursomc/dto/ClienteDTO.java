69e6623ee39fcb16e8aed19ef12cbf029ae18ead
pablo gonzalez <pablo949504563@gmail.com>
2020-02-03 09:32:41 -0300

    validação customizada: email não repetido  na atualização de cliente

--- src/main/java/com/delphos/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/delphos/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,8 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.delphos.cursomc.domain.Cliente;
-
+import com.delphos.cursomc.services.validation.ClienteUpdate;
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
