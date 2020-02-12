0f682ca99ac76ab1bf71c5cf8d70f14cd07b0bf7
pablo gonzalez <pablo949504563@gmail.com>
2020-02-12 20:53:53 -0300

    SubTotal e Total do pedido

--- src/main/java/com/delphos/cursomc/domain/Pedido.java
+++ src/main/java/com/delphos/cursomc/domain/Pedido.java
@@ -54,6 +54,14 @@ public class Pedido implements Serializable {
 	}
 
 
+	public double getValorTotal() {
+		double soma = 0.0;
+		for (ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
